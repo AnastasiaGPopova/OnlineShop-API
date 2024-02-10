ad9bef8ba11bd4d5f3351e7bcc3b2485fa718e7b
unknown <ana.popova.register@gmail.com>
2024-02-10 13:36:04 +0200

    installing dependencies

--- oath-react-client/src/index.js
+++ oath-react-client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+       domain= {process.env.OAUTH_DOMAIN}
+       cientId={process.env.OAUTH_CLIENT_ID}
+       redirectUri={window.location.orgin}
+    
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
