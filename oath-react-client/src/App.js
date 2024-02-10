a43f0c093cb1b1443da1733b5399b03362bef5c4
unknown <ana.popova.register@gmail.com>
2024-02-10 13:43:27 +0200

    Update App.js

--- oath-react-client/src/App.js
+++ oath-react-client/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useAuth0} from '@auth0/auth0-react'
 
 function App() {
+
+  const {loginWithPopup, loginWithRedirect,logout, user, isAuthenticated} = useAuth0()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Auth0 Authentication</h1>
+      <ul>
+        <li><button onClick={loginWithPopup}>Login with Popup</button></li>
+        <li><button onClick={loginWithRedirect}>Login with Redirect</button></li>
+        <li><button> onClick={logout}Logout</button></li>
+      </ul>
     </div>
   );
 }
